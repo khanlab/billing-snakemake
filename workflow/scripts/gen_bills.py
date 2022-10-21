df6819153a4961a127e833d6350564e081ee597d
Tristan Kuehn <tkuehn@uwo.ca>
2022-10-21 10:28:47 -0400

    Use docker and generate summary excel sheet

--- workflow/scripts/gen_bills.py
+++ workflow/scripts/gen_bills.py
@@ -6,3 +6,9 @@ billing.generate_all_pi_bills([snakemake.input[0],
                                snakemake.input[3]],
                               snakemake.config["quarter"],
                               "results")
+billing.summarize_all_pi_bills([snakemake.input[0],
+                               snakemake.input[1],
+                               snakemake.input[2],
+                               snakemake.input[3]],
+                              snakemake.config["quarter"],
+                              f"results/summary_{snakemake.config['quarter']}.xlsx")
