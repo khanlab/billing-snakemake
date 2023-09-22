03f1b331c4c66b13e90a90200ba5e51e9b337f2e
Tristan Kuehn <tkuehn@uwo.ca>
2023-09-22 15:29:43 -0400

    Use process_everything

--- workflow/scripts/gen_bills.py
+++ workflow/scripts/gen_bills.py
@@ -1,15 +1,14 @@
 from pathlib import Path
-from cbsserverbilling import billing
+from cbsserverbilling.main import process_everything
 
-out_dir = Path(snakemake.output["quarter_dir"])
+out_dir = Path(snakemake.output.quarter_dir)
 out_dir.mkdir()
-billing.generate_all_pi_bills(
-    [snakemake.input[0], snakemake.input[1], snakemake.input[2], snakemake.input[3]],
-    snakemake.wildcards["quarter"],
-    str(out_dir),
+process_everything(
+    snakemake.input.pi,
+    snakemake.input.user,
+    snakemake.input.user_update,
+    snakemake.input.storage_update,
+    snakemake.wildcards.quarter,
+    out_dir,
 )
-billing.summarize_all_pi_bills(
-    [snakemake.input[0], snakemake.input[1], snakemake.input[2], snakemake.input[3]],
-    snakemake.wildcards["quarter"],
-    str(out_dir / f"summary_{snakemake.wildcards['quarter']}.xlsx"),
-)
+
